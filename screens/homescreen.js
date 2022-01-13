85cef6cab9ca7782ce1e76ef63ae2a8e31d9621c
Little Jacob <joelnoumbi28@gmail.com>
2022-01-13 15:33:14 +0100

    gradient home page

---
+++ screens/homescreen.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+import {LinearGradient} from 'expo-linear-gradient'
+import ToggleComp from '../components/ToggleComp'
+import LoadedComp from '../components/LoadedComp'
+
+const Homescreen = () => {
+    return (    
+     <LinearGradient style={styles.container} colors={["rgba(28,32,44,0)", "#1c202c", "#1c202c"]} locations={[0, 0.40, 1]}>
+         <ToggleComp titleOne="Steam Guard" titleTwo="Confirmations" />
+         <Text style={styles.floutedText}>logged in as LittleJacob</Text>
+         <Text style={styles.codeText}>N5KCV</Text>
+         <LoadedComp />
+         <View style={{alignSelf: 'center', marginTop: 20}}>
+            <Text style={{color: "#d3d3d3", fontWeight: "400"}}>You’ll enter your code each time you enter your</Text>
+            <Text style={{color: "#d3d3d3", fontWeight: "400"}}>password to sign in to your Steam account.</Text>  
+         </View>
+         <View style={{alignSelf: 'center', marginTop: 20}}>
+            <Text style={{color: "#2fb4f1", fontWeight: "400"}}>Tip: If you don't share your PC, you can select</Text>
+            <Text style={{color: "#2fb4f1", fontWeight: "400"}}>Remember my password" when you sign in to </Text>  
+            <Text style={{color: "#2fb4f1", fontWeight: "400"}}>the PC client to enter your password and</Text>  
+            <Text style={{color: "#2fb4f1", fontWeight: "400"}}>authenticator code less often.</Text>  
+         </View>
+         
+     </LinearGradient>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        borderWidth: 1,
+    },
+    floutedText: {
+        alignSelf: 'center',
+        color: "#576574",
+        fontSize: 14,
+        fontWeight: "400",
+    },
+    codeText: {
+        color: "#fff",
+        fontSize: 55,
+        fontWeight: "400",
+        letterSpacing: 5.6,
+        alignSelf: "center",
+    }
+})
+
+export default Homescreen;
